bfd8fed1abb04d12d7a76a9faa1ad86b6b4270b9
Kysil Ivan <kysilivan1997@gmail.com>
2023-05-06 22:01:42 +0200

    +1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  bodyColor: document.querySelector('body'),
+};
+
+let colorId = 'null';
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  refs.startBtn.disabled = true;
+  colorId = setInterval(() => {
+    refs.bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  clearInterval(colorId);
+  refs.startBtn.disabled = false;
+}
