3d6dc760d9845ae067fe2ce0b4aa13a984e9592e
Solembum05 <119060176+Solembum05@users.noreply.github.com>
2023-05-06 20:02:20 +0000

    Deploying to gh-pages from @ Solembum05/goit-js-hw-09@bfd8fed1abb04d12d7a76a9faa1ad86b6b4270b9 🚀

---
+++ 01-color-switcher.9f2f855a.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),bodyColor:document.querySelector("body")};let o="null";t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,o=setInterval((()=>{t.bodyColor.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){clearInterval(o),t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.9f2f855a.js.map
